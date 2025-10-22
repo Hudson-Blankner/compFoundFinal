57d7ae457d91be5a6395f926cf0ab36ce4546cd8
Porter Schmidt <posc5487@colorado.edu>
2025-10-22 11:25:35 -0600

    rect

--- src/porter.js
+++ src/porter.js
@@ -1,8 +1,3 @@
-function setup() {
-  createCanvas(window.innerWidth-20, window.innerHeight-20);
-}
 function draw() {
-  background(220);
-  rect(100,100,100,100);
-  Text("Hello World", 150, 150);
+  rect(400,400,100,100);
 }
