d784bec79b8d98d51f4c545a32d38dd4591d2ac7
LuckyBonniwell <thatcher.bonniwell@colorado.edu>
2025-10-22 11:24:00 -0600

    hmm

--- src/lucky.js
+++ src/lucky.js
@@ -2,7 +2,6 @@ function setup() {
   createCanvas(window.innerWidth-20, window.innerHeight-20);
 }
 function draw() {
-  background(220);
   rect(200,200,100,100);
   Text("Hello World", 150, 150);
 }
