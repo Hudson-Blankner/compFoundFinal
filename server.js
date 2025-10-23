61c5ce7e0244f3140508a799a87eaaf8d25de953
LuckyBonniwell <thatcher.bonniwell@colorado.edu>
2025-10-23 00:43:37 -0600

    c

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// server.js
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// 👇 THIS LINE IS SUPER IMPORTANT
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
-
-  // Broadcast player movement
-  socket.on("playerMove", (data) => {
-    socket.broadcast.emit("playerMoved", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
 });
 
-const PORT = 3000;
-http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
