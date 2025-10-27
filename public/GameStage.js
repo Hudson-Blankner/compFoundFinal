17b154115a1ea907324b9e4ff634d089f5571a12
LuckyBonniwell <thatcher.bonniwell@colorado.edu>
2025-10-27 10:17:15 -0600

    gameboxes

--- public/GameStage.js
+++ public/GameStage.js
@@ -6,4 +6,15 @@ function gameChoice(){
     if (keyIsDown(UP_ARROW)) y -= 5;
     if (keyIsDown(DOWN_ARROW)) y += 5; 
     
+    fill(210);
+    rect(platGameBox.x,platGameBox.y,platGameBox.w,platGameBox.h,10)
+    rect(shipGameBox.x,shipGameBox.y,shipGameBox.w,shipGameBox.h,10)
+    rect(gunGameBox.x,gunGameBox.y,gunGameBox.w,gunGameBox.h,10)
+    rect(mazeGameBox.x,mazeGameBox.y,mazeGameBox.w,mazeGameBox.h,10)
+    rect(spaceGameBox.x,spaceGameBox.y,spaceGameBox.w,spaceGameBox.h,10)
+    rect(plinkGameBox.x,plinkGameBox.y,plinkGameBox.w,plinkGameBox.h,10)
+    rect(game7GameBox.x,game7GameBox.y,game7GameBox.w,game7GameBox.h,10)
+    rect(game8GameBox.x,game8GameBox.y,game8GameBox.w,game8GameBox.h,10)
+    rect(game9GameBox.x,game9GameBox.y,game9GameBox.w,game9GameBox.h,10)
+    rect(game10GameBox.x,game10GameBox.y,game10GameBox.w,game10GameBox.h,10)
 }
--- public/globablVar.js
+++ public/globablVar.js
@@ -12,12 +12,23 @@ function wipe(){
 //                  ==== Wipe Function ====
 
 //                  ==== Calling in Sounds and images ====
+
 //                  ==== Stage variables ====
 let titleSetup = true;
 let startButtonCount = 0;
 let stageCnt = 0;
 const startBoxW = 200; const startBoxH = 100;
 const startBox = {x: canvMid[0] -(startBoxW/2), y: canvMid[1], w: 200, h:100}
+const platGameBox = {x: 10, y: 10, w: 337.5, h:220}
+const shipGameBox = {x: 357.5, y: 10, w: 337.5, h:220}
+const gunGameBox = {x: 705, y: 10, w: 337.5, h:220}
+const mazeGameBox = {x: 1052.5, y: 10, w: 337.5, h:220}
+const spaceGameBox = {x: 1052.5, y: 240, w: 337.5, h:220}
+const plinkGameBox = {x: 1052.5, y: 470, w: 337.5, h:220}
+const game7GameBox = {x: 10, y: 240, w: 337.5, h:220}
+const game8GameBox = {x: 10, y: 470, w: 337.5, h:220}
+const game9GameBox = {x: 357.5, y: 470, w: 337.5, h:220}
+const game10GameBox = {x: 705, y: 470, w: 337.5, h:220}
 let titleTxt = []
 let playerOneStartX = startBox.x
 let playerOneStartY = 100
