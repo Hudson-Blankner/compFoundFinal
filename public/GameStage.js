335184864bf8ada79382b9a3e4a32bf810477f01
LuckyBonniwell <thatcher.bonniwell@colorado.edu>
2025-10-27 16:20:11 -0600

    mmmmm

--- public/GameStage.js
+++ public/GameStage.js
@@ -27,29 +27,34 @@ function gameChoice(){
     if (keyIsDown(DOWN_ARROW)) y += 5; 
     
     fill(210);
-    rect(platGameBox.x,platGameBox.y,platGameBox.w,platGameBox.h,10)
-    rect(shipGameBox.x,shipGameBox.y,shipGameBox.w,shipGameBox.h,10)
-    rect(gunGameBox.x,gunGameBox.y,gunGameBox.w,gunGameBox.h,10)
-    rect(mazeGameBox.x,mazeGameBox.y,mazeGameBox.w,mazeGameBox.h,10)
-    rect(spaceGameBox.x,spaceGameBox.y,spaceGameBox.w,spaceGameBox.h,10)
-    rect(plinkGameBox.x,plinkGameBox.y,plinkGameBox.w,plinkGameBox.h,10)
-    rect(tronGameBox.x,tronGameBox.y,tronGameBox.w,tronGameBox.h,10)
-    rect(tagGameBox.x,tagGameBox.y,tagGameBox.w,tagGameBox.h,10)
-    rect(huntGameBox.x,huntGameBox.y,huntGameBox.w,huntGameBox.h,10)
-    rect(tankGameBox.x,tankGameBox.y,tankGameBox.w,tankGameBox.h,10)
+    // rect(platGameBox.x,platGameBox.y,platGameBox.w,platGameBox.h,10)
+    // rect(shipGameBox.x,shipGameBox.y,shipGameBox.w,shipGameBox.h,10)
+    // rect(gunGameBox.x,gunGameBox.y,gunGameBox.w,gunGameBox.h,10)
+    // rect(mazeGameBox.x,mazeGameBox.y,mazeGameBox.w,mazeGameBox.h,10)
+    // rect(spaceGameBox.x,spaceGameBox.y,spaceGameBox.w,spaceGameBox.h,10)
+    // rect(plinkGameBox.x,plinkGameBox.y,plinkGameBox.w,plinkGameBox.h,10)
+    // rect(tronGameBox.x,tronGameBox.y,tronGameBox.w,tronGameBox.h,10)
+    // rect(tagGameBox.x,tagGameBox.y,tagGameBox.w,tagGameBox.h,10)
+    // rect(huntGameBox.x,huntGameBox.y,huntGameBox.w,huntGameBox.h,10)
+    // rect(tankGameBox.x,tankGameBox.y,tankGameBox.w,tankGameBox.h,10)
 
-checkBox(platGameBox, platGameBoxCharge, platGameBox, 1)
-checkBox(shipGameBox, shipGameBoxCharge, shipGameBox, 2)
-checkBox(gunGameBox, gunGameBoxCharge, gunGameBox, 3)
-checkBox(mazeGameBox, mazeGameBoxCharge, mazeGameBox, 4)
-checkBox(spaceGameBox, spaceGameBoxCharge, spaceGameBox, 5)
-checkBox(plinkGameBox, plinkGameBoxCharge, plinkGameBox, 6)
-checkBox(tronGameBox, tronGameBoxCharge, tronGameBox, 7)
-checkBox(tagGameBox, tagGameBoxCharge, tagGameBox, 8)
-checkBox(huntGameBox, huntGameBoxCharge, huntGameBox, 9)
-checkBox(tankGameBox, tankGameBoxCharge, tankGameBox, 10)
+checkBox(platGameBox, playersOnPlatCharge, playersOnPlat, 1)
+checkBox(shipGameBox, playersOnShipCharge, playersOnShip, 2)
+checkBox(gunGameBox, playersOnGunCharge, playersOnGun, 3)
+checkBox(mazeGameBox, playersOnMazeCharge, playersOnMaze, 4)
+checkBox(spaceGameBox, playersOnSpaceCharge, playersOnSpace, 5)
+checkBox(plinkGameBox, playersOnPlinkCharge, playersOnPlink, 6)
+checkBox(tronGameBox, playersOnTronCharge, playersOnTron, 7)
+checkBox(tagGameBox, playersOnTagCharge, playersOnTag, 8)
+checkBox(huntGameBox, playersOnHuntCharge, playersOnHunt, 9)
+checkBox(tankGameBox, playersOnTankCharge, playersOnTank, 10)
     function checkBox(box, charge, game, gamenum) {
+        fill(210)
+        rect(box.x,box.y,box.w,box.h,10)
+        fill(255)
+        rect(box.x+5,box.y+5,charge,box.h-10)
         for (let id in players) {
+            console.log(players[id].x,players[id].y);
             if (players[id].x > box.x && players[id].x < box.x+box.w && 
                 players[id].y > box.y && players[id].y < box.y+box.h) {
                 game += 1;
@@ -58,7 +63,7 @@ checkBox(tankGameBox, tankGameBoxCharge, tankGameBox, 10)
                 game -= 1;
             }
         }
-        if (playersOnStart >= 1) {
+        if (game >= 1) {
             charge += 2;
         }
         else {
