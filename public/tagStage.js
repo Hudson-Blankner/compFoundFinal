a4a6eecfedbe2482046de2d6c7b33290978315b6
Hudson-Blankner <hudson.blankner@colorado.edu>
2025-11-04 23:16:50 -0700

    yayaya

--- public/tagStage.js
+++ public/tagStage.js
@@ -4,4 +4,5 @@ function tagGame(){
     titleTxt.length = 0
     titleTxt.push(new titleText(canvMid[0],canvMid[1],"black",0,100, "Tag Game!"))
     titleTxt[0].draw();
+    //LORDE LORDE LORDE! YAYAYAYAY
 }
