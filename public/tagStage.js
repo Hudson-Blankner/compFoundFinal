77c68c3deba9360cd69b40fe5fb71cc026f35bc3
Hudson-Blankner <hudson.blankner@colorado.edu>
2025-11-04 23:03:50 -0700

    YAYAYA LORDE LORDE LORDE

--- public/tagStage.js
+++ public/tagStage.js
@@ -1,3 +1,7 @@
 function tagGame(){
     background(220)
+    let tagDoors = []
+    titleTxt.length = 0
+    titleTxt.push(new titleText(canvMid[0],canvMid[1],"black",0,100, "Tag Game!"))
+    titleTxt[0].draw();
 }
