b67154b48ef488031887311bf1848d9d742a180d
Hudson-Blankner <hudson.blankner@colorado.edu>
2025-11-12 10:30:22 -0700

    dd

--- public/players.js
+++ public/players.js
@@ -1,13 +1,15 @@
 class Player {
-    constructor(id, color, x = 100, y = 100, stage = 0){
+    constructor(id, color, x = 100, y = 100, r = 0, stage = 0){
         this.id = id;
         this.x = x;
         this.y = y;
+        this.r = r;
         this.stage = stage
         this.color = color || "white";
     }
-    move(x, y){
+    move(x, y, r){
         this.x = x;
         this.y = y;
+        this.r = r;
     }
 }
