585db6cfa2fa6326a245346ffef2f1769f524307
Hudson-Blankner <hudson.blankner@colorado.edu>
2025-11-17 09:46:51 -0700

    rarra

--- public/tagStage.js
+++ public/tagStage.js
@@ -1,5 +1,14 @@
 function tagGame(){
     background(220)
+    tagStageSetupCount = 1
+    if (tagStageSetupCount = 1) {
+        for (let id in players){
+            if (players[id].color = "blue") {x = 250}
+            if (players[id].color = "purple") {x = 350}
+            if (players[id].color = "orange") {x = 450}
+        }
+    tagStageSetupCount = 0
+    }
     let tagDoors = []
     let tagText = []
     titleTxt.length = 0
@@ -7,9 +16,9 @@ function tagGame(){
     tagText[0].draw();
     
     
-    // if (keyIsDown(LEFT_ARROW)) x -= 5;
-    // if (keyIsDown(RIGHT_ARROW)) x += 5;
-    // if (keyIsDown(UP_ARROW)) y -= 5;
-    // if (keyIsDown(DOWN_ARROW)) y += 5; 
+    if (keyIsDown(LEFT_ARROW)) x -= 5;
+    if (keyIsDown(RIGHT_ARROW)) x += 5;
+    if (keyIsDown(UP_ARROW)) y -= 5;
+    if (keyIsDown(DOWN_ARROW)) y += 5; 
     
 }
