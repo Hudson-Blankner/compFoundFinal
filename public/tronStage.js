c15912faad2cdbb7ecbee2b38f95a5c644cf6a6d
Porter Schmidt <posc5487@colorado.edu>
2025-12-04 21:13:28 -0700

    😩

--- public/tronStage.js
+++ public/tronStage.js
@@ -1,83 +1,76 @@
 function tronGame(){
-    let x0 = 400;
-let y0 = 400;
+  let x = 20
+  let y = 200
+  let direction = 'right'
 
-let trail = [];
+  let gameOver = false
 
-
-function setup() {
-  createCanvas(innerWidth, innerHeight);
-  player1 = new Player(x0, y0, 20, 20);
-}
-
-function draw() {
-  background('black');
-  player1.draw();
-
-  // Movin and Groovin
-  player1.move();
-
-  if (keyIsDown(UP_ARROW) === true) {
-    if (player1.direction !== 'down') {
-      player1.direction = 'up'
-    }
-  }
-  if (keyIsDown(RIGHT_ARROW) === true) {
-    if (player1.direction !== 'left') {
-      player1.direction = 'right'
-    }
-  }
-  if (keyIsDown(DOWN_ARROW) === true) {
-    if (player1.direction !== 'up') {
-      player1.direction = 'down'
-    }
-  }
-  if (keyIsDown(LEFT_ARROW) === true) {
-    if (player1.direction !== 'right') {
-      player1.direction = 'left'
-    }
+  function setup() {
+    createCanvas(innerWidth, innerHeight);
+    background('black');
+    frameRate(90);
   }
 
-  // Line thingy
-  push();
-  stroke('orange');
-  strokeWeight(4);
-  line(x0 + (player1.w / 2), y0 + (player1.h / 2), player1.x + (player1.w / 2), player1.y + (player1.h / 2));
-  pop();
-}
-
-function keyPressed() {
-
-}
-
-class Player{
-  constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-
-    this.direction = 'up'
-    this.alive = true
+  function draw() {
+  if (gameOver == true) {
+    return
+   }
+  player()
   }
   
-  draw() {
-    fill('white');
-    rect(this.x, this.y, this.w, this.h);
-  }
-
-  move() {
-    if (this.direction === 'up') {
-      this.y -= 3;
-    } else if (this.direction === 'right') {
-      this.x += 3;
-    } else if (this.direction === 'down') {
-      this.y += 3;
-    } else if (this.direction === 'left') {
-      this.x -= 3;
-    } else {
-      this.direction = 'up';
+  function player(){
+    if (notBlack(x,y) === true) {
+      fill(225)
+      text("Game Over\n You fuckin such :/", 200, 180)
+      gameOver = true
+      fill(255,0,0)
+      ellipse(x,y,20,20)
     }
+    push();
+    stroke('orange');
+    strokeWeight(3);
+    point(x, y);
+    pop();
+    if (direction === 'left'){
+       x = x - 2
+   }
+   if (direction === 'up') {
+      y = y - 2
+   }
+   if (direction === 'right') {
+     x = x + 2
+   }
+   if (direction === 'down') {
+     y = y + 2
+   }
   }
-}
-}
+  function keyPressed(){
+   if (keyIsDown(LEFT_ARROW) === true) {
+     if (direction !== 'right') {
+      direction = 'left'
+     }
+   }
+   if (keyIsDown(UP_ARROW) === true) {
+     if (direction !== 'down') {
+      direction = 'up'
+     }
+   }
+   if (keyIsDown(RIGHT_ARROW) === true) {
+     if (direction !== 'left') {
+      direction = 'right'
+     }
+   }
+   if (keyIsDown(DOWN_ARROW) === true) {
+     if (direction !== 'up') {
+      direction = 'down'
+     }
+   }
+  }
+   function notBlack(x,y) {
+     var a = get(x, y);
+     if (a[0] !== 0) return true;
+     else if (a[1] !== 0) return true;
+     else if (a[2] !== 0) return true;
+     return false;
+   }
+  
